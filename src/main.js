ab9fc544f14ddff39908eaefea5ce55b4027e081
RaviNayakCapgemini <47587859+RaviNayakCapgemini@users.noreply.github.com>
2019-02-13 14:27:29 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,27 +1,24 @@
 require('../node_modules/@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.css');
 
-var BlockSDK = require('blocksdk');
-if (window.self === window.top) {
-	document.body.innerText = 'This application is for use in the Salesforce Marketing Cloud Content Builder Editor only.';
-} else {
-	var sdk = new BlockSDK();
-	sdk.getContent(function (content) {
-		var quill = new Quill('#editor-container', {
-			theme: 'snow'
-		});
-		quill.root.innerHTML = content;
-		function saveText() {
-			var html = quill.root.innerHTML;
-			sdk.setContent(html);
-			sdk.setSuperContent('This is super content: ' + html);
+var SDK = require('blocksdk');
+var sdk = new SDK();
 
-			sdk.getData(function (data) {
-				var numberOfEdits = data.numberOfEdits || 0;
-				sdk.setData({
-					numberOfEdits: numberOfEdits + 1
-				});
-			});
-		}
+sdk.getContent(function (content) 
+{
+	var quill = new Quill('#editor-container', {theme: 'snow'});
+	quill.root.innerHTML = content;
+	function saveText() 
+	{
+		var html = quill.root.innerHTML;
+		sdk.setContent(html);
+		sdk.setSuperContent('This is super content: ' + html);
+
+		sdk.getData(function (data) 
+		{
+			var numberOfEdits = data.numberOfEdits || 0;
+			sdk.setData({numberOfEdits: numberOfEdits + 1});
+		});
+	}
 		quill.on('text-change', saveText);
-	});
-}
+});
+
